f9f9a428c44cf1a4feb31b1bad3dd302558f401e
Todd Strader <tstrader@hudson-trading.com>
2022-06-14 08:41:17 -0400

    copyright

--- test_regress/t/t_public_clk.cpp
+++ test_regress/t/t_public_clk.cpp
@@ -1,3 +1,11 @@
+// -*- mode: C++; c-file-style: "cc-mode" -*-
+//
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2022 by Todd Strader.
+// SPDX-License-Identifier: CC0-1.0
+
 // Test defines
 #define MAIN_TIME_MULTIPLIER 1
 #include <memory>
